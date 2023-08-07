0edecfa3931cffee025850d8878a8c06fdaf2d9d
Odissey <orxan.aslanov.200212@gmail.com>
2023-08-07 14:45:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IlbizGoTask
+{
+    public class Ilbiz
+    {
+        public int Height;
+        public int Go = 5;
+        public int Back = 4;
+        public int Day = 0;
+
+        public int GetIlbiz(int height)
+        {
+            Height = height;
+            int sum = 0;
+
+            while (sum < Height)
+            {
+                sum += Go;
+              
+                Day++;
+
+                if (sum >= Height)
+                    break;
+                sum -= Back;
+            }
+
+            return Day;
+        }
+
+        static void Main(string[] args)
+        {
+            Ilbiz ilbiz = new Ilbiz();
+            Console.WriteLine(ilbiz.GetIlbiz(20));//15+5 metr   15+1//gun
+        }
+    }
+}
